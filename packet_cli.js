9b4ddeb61456740fa9ca86e82ce4b3c9534ad46b
Andy Cate <and164v@gmail.com>
2021-09-20 10:53:07 -0700

    update packet cli to work with Ethernet

--- packet_cli.js
+++ packet_cli.js
@@ -1,10 +1,17 @@
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
+const dgram = require('dgram');
+
+const server = dgram.createSocket('udp4');
+
 const rl = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// address of board to send to
+const BOARD_ADDRESS = "10.0.0.42";
+const BOARD_PORT = 42069;
+
+// checksum generator
 fletcher16 = (data) => {
   var a = 0, b = 0;
   for (var i = 0; i < data.length; i++) {
@@ -14,20 +21,25 @@ fletcher16 = (data) => {
   return a | (b << 8);
 }
 
-const port = new SerialPort(process.argv[2], {
-  baudRate: 57600,
-  autoOpen: false
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
 });
-
-port.open(() => {
-  const parser = new Readline();
-  port.pipe(parser);
-  parser.on('data', (data) => {
-    console.log(data);
-  });
+server.on('message', (msg, rinfo) => {
+  console.log(msg.toString());
+});
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+  if(process.platform === 'win32') {
+    server.send("big yeet", 42069, "10.0.0.42");
+  }
   rl.on('line', line => {
     const checksum = fletcher16(Buffer.from(line, 'binary')).toString(16);
-    port.write('{' + line + '|' + checksum + '}\n');
-  })
+    console.log('sending: ' + '{' + line + '|' + checksum + '}');
+    server.send('{' + line + '|' + checksum + '}', BOARD_PORT, BOARD_ADDRESS);
+  });
 });
+server.bind(42069);
 
+// Prints: server listening 0.0.0.0:42069
