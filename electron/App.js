50fcd9dbc6cf4605162266a6dda27d13d73cc818
Andy Cate <and164v@gmail.com>
2021-11-13 15:42:18 -0800

    stuff

--- electron/App.js
+++ electron/App.js
@@ -9,6 +9,7 @@ const DAQ = require('./DAQ');
 const DAQV3 = require('./DAQV3');
 const ActuatorController = require('./ActuatorController');
 const InfluxDB = require('./InfluxDB');
+const SerPort = require('./SerPort');
 
 class App {
   constructor() {
@@ -29,6 +30,7 @@ class App {
    */
   initApp(){
     this.port = new UdpPort('0.0.0.0', 42069, this.updateState);
+    this.port2 = new SerPort('/dev/yeet', 115200, this.updateState);
 
     this.flightComputer = new FlightV2(this.port,
       '10.0.0.42',
@@ -205,6 +207,11 @@ class App {
       () => this.updateState(Date.now(), { actCtrlr3Connected: true }),
       () => this.updateState(Date.now(), { actCtrlr3Connected: false }),
       (rate) => this.updateState(Date.now(), { actCtrlr3Kbps: rate }));
+
+    this.ereg = new EReg(this.port2, {},
+      () => this.updateState(Date.now(), { eregConnected: true }),
+      () => this.updateState(Date.now(), { eregConnected: false }),
+      (rate) => this.updateState(Date.now(), { eregKbps: rate }));
 
     this.setupIPC();
   }
