c056dd7b70b48afa1f00c8e451b6e52f5d7e4a36
Andy Cate <and164v@gmail.com>
2021-11-15 18:42:23 -0800

    working on it

--- electron/SerPort.js
+++ electron/SerPort.js
@@ -2,6 +2,7 @@ const dgram = require('dgram');
 
 const Packet = require('./Packet');
 const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
 
 class SerPort {
   /**
@@ -19,45 +20,9 @@ class SerPort {
     this.boards = {};
     this.updateStateCallback = updateStateCallback;
 
-    this.server.on('error', (err) => {
-      console.log(`${this.address}:${this.port} server error:\n${err.stack}`);
-      this.server.close();
+    this.lineStream.on('data', (msg) => {
+      
     });
-
-    this.server.on('message', (msg, rinfo) => {
-      let b = this.boards[rinfo.address];
-      if (b === undefined && rinfo.address !== '127.0.0.1') {
-        // enables testing packets from localhost
-        console.info(`received packet from unknown remote: ${rinfo.address}`)
-        return;
-      }
-      let pkt = Packet.parsePacket(msg.toString());
-      if (b === undefined && rinfo.address === '127.0.0.1') {
-        b = this.boards[`10.0.0.${pkt.values[0]}`] // if packet is from local test, set b to given address
-      }
-      const _pkt = b.packets[pkt?.id]
-      if (_pkt && Object.keys(_pkt).map(_k => _pkt[_k]).reduce((acc, cur) => acc || cur.parseAsString, false)) {
-        // should be parsed as string
-        pkt = Packet.parsePacket(msg.toString(), true)
-      }
-
-      if (rinfo.address === '127.0.0.1') {
-        pkt.values.shift() // discard first value from local testing packet
-      }
-
-      if (pkt) {
-        const update = b.processPacket(pkt);
-        if (update === undefined) return;
-        this.updateStateCallback(pkt.timestamp, update);
-      }
-    });
-
-    this.server.on('listening', () => {
-      const address = this.server.address();
-      console.log(`server listening ${address.address}:${address.port}`);
-    });
-
-    this.server.bind(this.port, this.address);
   }
 
   /**
