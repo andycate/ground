85a8e115ac914be9ca45c23246980804c01bc917
Curtis Hu <43627632+curtisjhu@users.noreply.github.com>
2022-10-06 23:31:51 -0700

    added files: requires massage

---
+++ telemetry/src/components/Map.js
@@ -0,0 +1,193 @@
+import React, { useEffect, useState } from 'react';
+
+import { withStyles, withTheme } from '@material-ui/core/styles';
+import { FormControl, Grid, MenuItem, Select } from '@material-ui/core';
+import ReactMapGL, {
+  AttributionControl,
+  FlyToInterpolator,
+  Layer, Marker,
+  ScaleControl,
+  Source
+} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import comms from '../api/Comms';
+import { Room } from "@material-ui/icons";
+
+const styles = theme => ({
+  root: {
+    height: '100%',
+    margin: 0
+  },
+  fullHeight: {
+    height: '100%',
+    padding: "0px !important"
+  }
+});
+
+const mapStyles = [
+  { name: "Light", url: "mapbox://styles/mapbox/light-v10" },
+  { name: "Dark", url: "mapbox://styles/mapbox/dark-v10" },
+  { name: "Satellite", url: "mapbox://styles/mapbox/satellite-v9" }
+]
+
+const defaultLong = -117.80822750160537
+const defaultLat = 35.34737384872146
+
+let nextLat = 35.34737384872146;
+
+function CurrentCoords(props) {
+  const { longitude, latitude } = props;
+
+  const markerStyle = {
+    position: 'absolute',
+    background: 'rgba(255,255,255,0.45)',
+    left: 0,
+    top: 0,
+    padding: 5
+  };
+
+  return (
+    <div style={markerStyle}>
+      Current Position: ({longitude.toFixed(7)}, {latitude.toFixed(7)})
+    </div>
+  );
+}
+
+function SwitchStyle({ setSelectedStyle, selectedStyle }) {
+  const markerStyle = {
+    position: 'absolute',
+    background: 'rgba(255,255,255,0.45)',
+    right: 0,
+    minWidth: 80,
+    top: 0
+  };
+
+  return (
+    <div style={markerStyle}>
+      <FormControl fullWidth>
+        <Select
+          id="demo-simple-select"
+          value={selectedStyle}
+          label="Map Style"
+          disableUnderline
+          onChange={(evt) => {
+            setSelectedStyle(evt.target.value)
+          }}
+        >
+          {
+            mapStyles.map(style => <MenuItem key={style.url} value={style.url}>{style.name}</MenuItem>)
+          }
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+function Map({ field, classes }) {
+  const [selectedStyle, setSelectedStyle] = useState(mapStyles[0].url)
+  const [viewport, setViewport] = useState({
+    longitude: defaultLong,
+    latitude: defaultLat,
+    zoom: 11
+  });
+
+  const [coordinateHistory, _setCoordinateHistory] = useState([[defaultLong, defaultLat]])
+
+  const historyLength = coordinateHistory.length
+  const rocketLong = coordinateHistory[historyLength - 1][0]
+  const rocketLat = coordinateHistory[historyLength - 1][1]
+
+  const geoJSON = {
+    type: 'Feature',
+    properties: {},
+    geometry: { type: 'LineString', coordinates: coordinateHistory }
+  }
+
+  const trailLayer = {
+    id: "rocket_trail",
+    type: "line",
+    layout: {
+      "line-join": "round",
+      "line-cap": "round"
+    },
+    source: "rocket-trail-data",
+    paint: {
+      "line-color": "#ff6565",
+      "line-width": 2
+    }
+  }
+
+  useEffect(() => {
+    if (!process.env.REACT_APP_MAPBOX_ACCESS_TOKEN) {
+      window.alert("You are missing the REACT_APP_MAPBOX_ACCESS_TOKEN environment variable, please obtain a public token then put it in a .env file")
+    }
+  }, [])
+
+  useEffect(() => {
+    comms.addSubscriber('gpsLatitude', handleLatUpdate);
+    comms.addSubscriber('gpsLongitude', handleLongUpdate);
+    comms.addDarkModeListener(handleDarkMode);
+    return () => {
+      comms.removeSubscriber('gpsLatitude', handleLatUpdate);
+      comms.removeSubscriber('gpsLongitude', handleLongUpdate);
+      comms.removeDarkModeListener(handleDarkMode)
+    }
+  }, [])
+
+  const handleDarkMode = (isDark) => {
+    if(isDark){
+      setSelectedStyle(mapStyles.find(({name}) => name === "Dark").url)
+    }else{
+      setSelectedStyle(mapStyles.find(({name}) => name === "Light").url)
+    }
+  }
+
+  function handleLatUpdate(timestamp, data) {
+    if(data !== 0) {
+      nextLat = data;
+    }
+  }
+
+  function handleLongUpdate(timestamp, data) {
+    if(data !== 0 && nextLat !== 0) {
+      setViewport(_viewport => ({
+        ..._viewport,
+        longitude: data,
+        latitude: nextLat,
+        transitionDuration: 500,
+        transitionInterpolator: new FlyToInterpolator()
+      }));
+      // TODO: depending on rate of data, may need to reduce / simplify path
+      _setCoordinateHistory(prev => ([...prev, [data, nextLat]]))
+    }
+  }
+
+  return (
+    <Grid container spacing={1} alignItems='center' className={classes.root}>
+      <Grid item xs={12} className={classes.fullHeight}>
+        <ReactMapGL
+          {...viewport}
+          width={"100%"}
+          height={"100%"}
+          onViewportChange={nextViewport => setViewport(nextViewport)}
+          attributionControl={false}
+          mapStyle={selectedStyle}
+        >
+          <SwitchStyle setSelectedStyle={setSelectedStyle} selectedStyle={selectedStyle}/>
+          <CurrentCoords longitude={rocketLong} latitude={rocketLat}/>
+          <Marker longitude={rocketLong} latitude={rocketLat} offsetLeft={-17.5} offsetTop={-30}>
+            <Room fontSize={"large"} htmlColor={selectedStyle.includes("dark") ? "#d94848" : "#000"}/>
+          </Marker>
+          <Source type={"geojson"} id={"rocket-trail-data"} data={geoJSON}>
+            <Layer {...trailLayer}/>
+          </Source>
+          <ScaleControl maxWidth={200} unit="imperial" style={{ right: 50, bottom: 10, opacity: 0.7 }}/>
+          <AttributionControl compact={true} style={{ right: 0, bottom: 0 }}/>
+        </ReactMapGL>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default withTheme(withStyles(styles)(Map));
---
+++ telemetry/src/components/RocketOrientation.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+
+import * as THREE from 'three';
+import OrbitControlsFactory from 'three-orbit-controls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import { withStyles, withTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+import comms from '../api/Comms';
+
+const OrbitControls = OrbitControlsFactory(THREE);
+
+const styles = theme => ({
+  root: {
+    height: '100%'
+  },
+  cardContent: {
+    padding: 0,
+    paddingBottom: "0 !important",
+    height: '100%',
+  },
+  button: {
+    float: 'right',
+    minWidth: '0px',
+    zIndex: 1000
+  },
+  canvas: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  },
+  sizeDetector: {
+    position: 'relative',
+    width: '100%',
+    height: '100%'
+  },
+  legend: {}
+});
+
+class RocketOrientation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.sizeDetector = React.createRef();
+    this.lad4gltf = null
+
+    this.qW = 0.0;
+    this.qX = 0.0;
+    this.qY = 0.0;
+    this.qZ = 0.0;
+
+    this.handleValueUpdate = this.handleValueUpdate.bind(this)
+    this.handleqWUpdate = this.handleqWUpdate.bind(this)
+    this.handleqXUpdate = this.handleqXUpdate.bind(this)
+    this.handleqYUpdate = this.handleqYUpdate.bind(this)
+  }
+
+  componentDidMount() {
+    const { fieldQW, fieldQX, fieldQY, fieldQZ } = this.props;
+
+    const width = this.sizeDetector.current.clientWidth;
+    const height = this.sizeDetector.current.clientHeight;
+    let scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000);
+    let renderer = new THREE.WebGLRenderer({ alpha: true });
+    let controls = new OrbitControls(camera, renderer.domElement);
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor(0x000000, 0.9);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    this.sizeDetector.current.appendChild(renderer.domElement);
+    const axesHelper = new THREE.AxesHelper(2);
+    scene.add(axesHelper);
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    scene.add(directionalLight);
+
+    const loader = new GLTFLoader();
+    const that = this
+    loader.load('static/3d-models/LAD4.gltf', function (gltf) {
+      that.lad4gltf = gltf;
+      scene.add(gltf.scene);
+      gltf.scene.setRotationFromAxisAngle(new THREE.Vector3(0,1,0), Math.PI / 2)
+    }, undefined, function (error) {
+      console.error(error);
+    });
+
+    camera.position.x = 3;
+    camera.position.y = 3;
+    camera.position.z = 3;
+    camera.lookAt(0, 0, 0);
+    controls.update();
+
+    let animate = () => {
+      this.animationHandle = requestAnimationFrame(animate);
+
+      controls.update();
+
+      directionalLight.position.x = camera.position.x;
+      directionalLight.position.y = camera.position.y;
+      directionalLight.position.z = camera.position.z;
+
+      renderer.render(scene, camera);
+    };
+    animate();
+
+    comms.addSubscriber(fieldQW, this.handleqWUpdate);
+    comms.addSubscriber(fieldQX, this.handleqXUpdate);
+    comms.addSubscriber(fieldQY, this.handleqYUpdate);
+    comms.addSubscriber(fieldQZ, this.handleValueUpdate); // actuall update the model once QZ received
+  }
+
+  handleqWUpdate(timestamp, data) {this.qW = data}
+  handleqXUpdate(timestamp, data) {this.qX = data}
+  handleqYUpdate(timestamp, data) {this.qY = data}
+
+  handleValueUpdate(timestamp, data) {
+    this.qZ = data;
+    if (!this.lad4gltf) return
+
+    let quat = new THREE.Quaternion(this.qW, this.qX, this.qY, this.qZ);
+    // const rot1 = new THREE.Quaternion(-Math.sqrt(2) / 2, 0, 0, Math.sqrt(2) / 2);
+    // const rot2 = new THREE.Quaternion(0, 0, Math.sqrt(2) / 2, Math.sqrt(2) / 2);
+    // quat = quat.premultiply(new THREE.Quaternion(0, Math.sqrt(2)/2, 0, Math.sqrt(2)/2));
+    // quat = quat.multiply((new THREE.Quaternion(0, Math.sqrt(2)/2, 0, Math.sqrt(2)/2)).invert());
+    // quat = quat.premultiply(rot1);
+    // quat = quat.multiply(rot1.invert());
+    // quat = quat.premultiply(rot2);
+    // quat.multiply(rot2.invert());
+    this.lad4gltf.scene.setRotationFromQuaternion(quat);
+    // lad4gltf.scene.setRotationFromEuler(new THREE.Qua(Math.PI * data[0] / 180, Math.PI * data[1] / 180, Math.PI * data[2] / 180, "YXZ"));
+  }
+
+  componentWillUnmount() {
+    const { fieldQW, fieldQX, fieldQY, fieldQZ } = this.props;
+    comms.removeSubscriber(fieldQW, this.handleqWUpdate);
+    comms.removeSubscriber(fieldQX, this.handleqXUpdate);
+    comms.removeSubscriber(fieldQY, this.handleqYUpdate);
+    comms.removeSubscriber(fieldQZ, this.handleValueUpdate);
+    cancelAnimationFrame(this.animationHandle);
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Card className={classes.root}>
+        <CardContent className={classes.cardContent}>
+          <div ref={this.sizeDetector} className={classes.sizeDetector}>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+export default withTheme(withStyles(styles)(RocketOrientation));
