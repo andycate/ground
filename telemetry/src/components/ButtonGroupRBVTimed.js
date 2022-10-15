5d6bebea7ab044cf281554cd69dd2146fe7f5da4
Curtis Hu <43627632+curtisjhu@users.noreply.github.com>
2022-10-15 13:58:59 -0700

    updates on main

--- telemetry/src/components/ButtonGroupRBVTimed.js
+++ telemetry/src/components/ButtonGroupRBVTimed.js
@@ -1,55 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { withStyles, withTheme } from '@material-ui/core/styles';
-import { Grid, Button, Box, TextField } from '@material-ui/core';
+import { withStyles, withTheme } from "@material-ui/core/styles";
+import { Grid, Button, Box, TextField } from "@material-ui/core";
 
-import comms from '../api/Comms';
+import comms from "../api/Comms";
 
-const styles = theme => ({
+const styles = (theme) => ({
   spacer: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   openButton: {
-    backgroundColor: theme.palette.success.main + ' !important',
-    color: theme.palette.text.primary + ' !important',
-    borderColor: theme.palette.success.main + ' !important',
-    transition: 'none'
+    backgroundColor: theme.palette.success.main + " !important",
+    color: theme.palette.text.primary + " !important",
+    borderColor: theme.palette.success.main + " !important",
+    transition: "none",
   },
   openButtonOutline: {
-    color: theme.palette.success.main + ' !important',
-    borderColor: theme.palette.success.main + ' !important',
-    transition: 'none'
+    color: theme.palette.success.main + " !important",
+    borderColor: theme.palette.success.main + " !important",
+    transition: "none",
   },
   closedButton: {
-    backgroundColor: theme.palette.error.main + ' !important',
-    color: theme.palette.text.primary + ' !important',
-    transition: 'none'
+    backgroundColor: theme.palette.error.main + " !important",
+    color: theme.palette.text.primary + " !important",
+    transition: "none",
   },
   closedButtonOutline: {
-    color: theme.palette.error.main + ' !important',
-    transition: 'none'
+    color: theme.palette.error.main + " !important",
+    transition: "none",
   },
   openStatusBox: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.primary,
-    backgroundColor: theme.palette.success.main
+    backgroundColor: theme.palette.success.main,
   },
   closedStatusBox: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.primary,
-    backgroundColor: theme.palette.error.main
+    backgroundColor: theme.palette.error.main,
   },
   txtField: {
-    width: '4rem'
-  }
+    width: "4rem",
+  },
 });
 
 const statusBox = {
-  borderColor: 'text.secondary',
+  borderColor: "text.secondary",
   border: 0.5,
-  style: { width: '9rem', height: '1rem', marginLeft: 'auto', marginRight: 'auto' },
+  style: {
+    width: "9rem",
+    height: "1rem",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
 };
 
 class ButtonGroupRBVTimed extends Component {
@@ -69,22 +74,22 @@ class ButtonGroupRBVTimed extends Component {
   }
 
   updateStatus(timestamp, value) {
-    this.setState({status: value});
+    this.setState({ status: value });
   }
 
   handleTimeFieldChange(e) {
-    this.setState({timeField: parseFloat(e.target.value)});
+    this.setState({ timeField: parseFloat(e.target.value) });
   }
 
   setOpen() {
     const { open } = this.props;
-    this.setState({openClicked: true});
+    this.setState({ openClicked: true });
     open();
   }
 
   setClosed() {
     const { close } = this.props;
-    this.setState({openClicked: false});
+    this.setState({ openClicked: false });
     close();
   }
 
@@ -108,7 +113,7 @@ class ButtonGroupRBVTimed extends Component {
     const { classes, theme, text, noClose } = this.props;
     const { status, openClicked, timeField } = this.state;
     let sColor = null;
-    switch(status) {
+    switch (status) {
       case 0:
         sColor = theme.palette.error.main;
         break;
@@ -120,63 +125,84 @@ class ButtonGroupRBVTimed extends Component {
         break;
     }
     return (
-      <Grid container spacing={1} alignItems='center' direction="column" style={{textAlign: 'center'}}>
+      <Grid
+        container
+        spacing={1}
+        alignItems="center"
+        direction="column"
+        style={{ textAlign: "center" }}
+      >
         <Grid item xs={12}>
-          <Box component="span" display="block">{text}</Box>
+          <Box component="span" display="block">
+            {text}
+          </Box>
         </Grid>
         <Grid item xs={12}>
-          <Box borderRadius={4} {...statusBox} bgcolor={sColor}/>
+          <Box borderRadius={4} {...statusBox} bgcolor={sColor} />
         </Grid>
-        <Grid item>
-          <TextField
-            type='number'
-            step={10}
-            value={timeField}
-            onChange={this.handleTimeFieldChange}
-            className={classes.txtField}
-            inputProps={{
-              step: 50
-            }}
-          />
-          <Button
-            color='primary'
-            variant='contained'
-            onClick={this.setTime}
-            disabled={this.props.disabled || false}
-            disableRipple
-            size='small'
-          >
-            {this.props.failText || "Send"}
-          </Button>
-          {!noClose ? 
+        <Grid container item xs={12} columnSpacing={0}>
+          <Grid item xs={6}>
+            <TextField
+              type="number"
+              step={10}
+              value={timeField}
+              onChange={this.handleTimeFieldChange}
+              className={classes.txtField}
+              inputProps={{
+                step: 50,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
             <Button
-              color='secondary'
-              variant='outlined'
-              className={!openClicked ? classes.closedButton : classes.closedButtonOutline}
-              onClick={this.setClosed}
+              color="primary"
+              variant="contained"
+              onClick={this.setTime}
               disabled={this.props.disabled || false}
               disableRipple
-              size='small'
+              size="small"
             >
-              {this.props.failText || "Close"}
+              {this.props.failText || "Send"}
             </Button>
-            :
-            <></>
-          }
-          <Button
-            color='primary'
-            variant='outlined'
-            className={openClicked ? classes.openButton : classes.openButtonOutline}
-            onClick={this.setOpen}
-            disabled={this.props.disabled || false}
-            disableRipple
-            size='small'
-          >
-            {this.props.successText || "Open"}
-          </Button>
+          </Grid>
+          <Grid item xs={6}>
+            {!noClose ? (
+              <Button
+                color="secondary"
+                variant="outlined"
+                className={
+                  !openClicked
+                    ? classes.closedButton
+                    : classes.closedButtonOutline
+                }
+                onClick={this.setClosed}
+                disabled={this.props.disabled || false}
+                disableRipple
+                size="small"
+              >
+                {this.props.failText || "Close"}
+              </Button>
+            ) : (
+              <></>
+            )}
+          </Grid>
+          <Grid item xs={6}>
+            <Button
+              color="primary"
+              variant="outlined"
+              className={
+                openClicked ? classes.openButton : classes.openButtonOutline
+              }
+              onClick={this.setOpen}
+              disabled={this.props.disabled || false}
+              disableRipple
+              size="small"
+            >
+              {this.props.successText || "Open"}
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-        </Grid>
+        <Grid item></Grid>
         {/* <Grid item >
           <TextField
             type='number'
